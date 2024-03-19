59a48e7418afaed0d5e37820110837176ec8fefe
Pedro Corrêa <142067181+pedro-correax@users.noreply.github.com>
2024-03-18 20:28:31 -0400

    Update index.js

--- Prova1CPW2/js/index.js
+++ Prova1CPW2/js/index.js
@@ -1,22 +1,45 @@
 const btnCalculate = document.querySelector('.btn-calculate');
 const inputsList = document.querySelectorAll('.client-type input[type=radio]');
+let clientType = null;
+let error = false;
 
-function calculateInterest() {
-     const value = document.querySelector('#value'); //valor
-     const interestRate = document.querySelector('#interestRate')  //taxa juros
-     const clientType = document.querySelector('#client')
+function calculateInterest(clientType) {
+     const valueElementInput = document.querySelector('#values').value; //valor
+     const portionElementInput = document.querySelector('.portion').value;  //taxa juros
+     const result = document.querySelector('.result');
+     if (clientType == 'standard') {
+          interestRate = 2.5;
+     }
+     if (clientType == 'platinum') {
+          interestRate = 1.99;
+     }
+     if (clientType == 'gold') {
+          interestRate = 1.2;
+     }
+
+     const amount = valueElementInput * (1 + ((interestRate/100) * portionElementInput));
+     const portion = amount / portionElementInput;
+
+     result.innerText = 
+          `Valor total a ser pago: R$${amount.toFixed(2)} \n
+          Valor de cada parcela R$${portion.toFixed(2)}`;
+     
 
 }
 
-function returnClientType(){
+
+function getClientType(){
      inputsList.forEach((itemList) => {
           if (itemList.checked) {
-               return itemList.id;
+               clientType = itemList.id;
           }
      })
+
 }
 
-btnCalculate.addEventListener('click', () => {
 
-     console.log(returnClientType())
+btnCalculate.addEventListener('click', () => {
+     getClientType()
+     calculateInterest(clientType)
+     error = false;
 })
